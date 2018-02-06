3d8a4997eda7b712b064af230165bb814af45c1c
Emily Curtin <emily@framebit.org>
2018-02-06 15:30:43 -0500

    whoops

--- bin/out-test-submit.sh
+++ bin/out-test-submit.sh
@@ -3,7 +3,7 @@
 hdfs dfs -rmdir /tmp/out-test.csv
 
 "$SPARK_HOME"/bin/spark-submit \
-  --class "org.framebit.sparkpi.SparkPi" \
+  --class "org.framebit.sparkpi.OutputTest" \
   target/scala-2.11/SparkPi-assembly-0.1.0-SNAPSHOT.jar \
   hdfs:///tmp/out-test.csv
 
--- bin/shuffle-test-submit.sh
+++ bin/shuffle-test-submit.sh
@@ -3,3 +3,4 @@
 "$SPARK_HOME"/bin/spark-submit \
   --class "org.framebit.sparkpi.ShuffleTest" \
   target/scala-2.11/SparkPi-assembly-0.1.0-SNAPSHOT.jar
+  10000
