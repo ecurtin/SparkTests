cda792ecc0c23dc2918c7db43277487d4eef115c
Emily Curtin <emily@framebit.org>
2018-02-07 11:03:27 -0500

    updates to shuffle test scripts

---
+++ bin/shuffle-test-1000000-4g-18core.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+"$SPARK_HOME"/bin/spark-submit \
+  --class org.framebit.sparkpi.ShuffleTest \
+  --executor-memory 4g \
+  --total-executor-cores 18 \
+  target/scala-2.11/SparkPi-assembly-0.1.0-SNAPSHOT.jar \
+  1000000
--- bin/shuffle-test-submit.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-"$SPARK_HOME"/bin/spark-submit \
-  --class "org.framebit.sparkpi.ShuffleTest" \
-  target/scala-2.11/SparkPi-assembly-0.1.0-SNAPSHOT.jar
-  10000
